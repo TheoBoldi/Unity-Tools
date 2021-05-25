eca5d04dd5335b638278e7cd2662582d63afd129
Boldi <theo.boldireff@edu.devinci.fr>
2021-05-25 14:38:54 +0200

    Project commit

---
+++ EditorTools/Assets/Rendu/Scripts/Editor/AnimationSimulationWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimationSimulationWindow : EditorWindow
+{
+    bool groupEnabled;
+    bool myBool = true;
+    float myFloat = 1.23f;
+
+    public string[] options;
+    public Animator[] animatorsList;
+
+    [MenuItem("Window/Animation Simluation Window")]
+
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(AnimationSimulationWindow));
+    }
+
+    void OnGUI()
+    {
+        #region animatorlist
+        GUILayout.Label("Animators List", EditorStyles.boldLabel);
+        animatorsList = GameObject.FindObjectsOfType<Animator>();
+
+        string count = EditorGUILayout.TextField("Number of elements :", animatorsList.Length.ToString());
+
+        for (int i = 0; i < animatorsList.Length; i++)
+        {
+            animatorsList[i] = (Animator)EditorGUILayout.ObjectField("Element " + i + " :", animatorsList[i], typeof(Animator), true);
+        }
+
+        #endregion
+
+        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+        myBool = EditorGUILayout.Toggle("Toggle", myBool);
+        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
+        EditorGUILayout.EndToggleGroup();
+    }
+}
---
+++ EditorTools/Assets/Scripts/Editor/Inspectors/GameManagerInspector.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(GameManager))]
+public class GameManagerInspector : Editor
+{
+    private static readonly string[] EXCLUDED_PROPERTIES = new string[]
+    {
+        "m_Script",
+        "gameData"
+    };
+
+    private void OnEnable()
+    {
+        SerializedProperty gameDataProperty = serializedObject.FindProperty("gameData");
+        if (null == gameDataProperty.objectReferenceValue) {
+            serializedObject.Update();
+            gameDataProperty.objectReferenceValue = _FindGameDataInProject();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+
+    private GameData _FindGameDataInProject()
+    {
+        string[] fileGuidsArr = AssetDatabase.FindAssets("t:" + typeof(GameData));
+        if (fileGuidsArr.Length > 0) {
+            string assetPath = AssetDatabase.GUIDToAssetPath(fileGuidsArr[0]);
+            return AssetDatabase.LoadAssetAtPath<GameData>(assetPath);
+        } else {
+            GameData gameData = ScriptableObject.CreateInstance<GameData>();
+            AssetDatabase.CreateAsset(gameData, "Assets/GameData.asset");
+            AssetDatabase.SaveAssets();
+            return gameData;
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("gameData"));
+        EditorGUI.EndDisabledGroup();
+
+        //Standard Editor Space
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Edit GameData")) {
+            Selection.activeObject = serializedObject.FindProperty("gameData").objectReferenceValue;
+        }
+
+        //Custom Space
+        GUILayout.Space(20);
+
+        DrawPropertiesExcluding(serializedObject, EXCLUDED_PROPERTIES);
+    }
+}
